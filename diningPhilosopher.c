2bd8f2ad4b990c7e4bb77a4fae28292f8c5e6ec3
Bhavin Patil <59190105+bhavinpatil@users.noreply.github.com>
2022-12-11 22:24:58 +0530

    Update diningPhilosopher.c

--- diningPhilosopher.c
+++ diningPhilosopher.c
@@ -1,124 +1,66 @@
-#include <pthread.h>
-#include <semaphore.h>
 #include <stdio.h>
+int p[5];
+int chop[5];
 
-#define N 5
-#define THINKING 2
-#define HUNGRY 1
-#define EATING 0
-#define LEFT (phnum + 4) % N
-#define RIGHT (phnum + 1) % N
-
-int state[N];
-int phil[N] = { 0, 1, 2, 3, 4 };
-
-sem_t mutex;
-sem_t S[N];
-
-void test(int phnum)
+void signal(int z)
 {
-	if (state[phnum] == HUNGRY
-		&& state[LEFT] != EATING
-		&& state[RIGHT] != EATING) {
-		// state that eating
-		state[phnum] = EATING;
-
-		sleep(2);
-
-		printf("Philosopher %d takes fork %d and %d\n",
-					phnum + 1, LEFT + 1, phnum + 1);
-
-		printf("Philosopher %d is Eating\n", phnum + 1);
-
-		// sem_post(&S[phnum]) has no effect
-		// during takefork
-		// used to wake up hungry philosophers
-		// during putfork
-		sem_post(&S[phnum]);
-	}
+    p[z] = 0;
+    chop[z] = 0;
+    chop[(z + 1) % 5] = 0;
 }
 
-// take up chopsticks
-void take_fork(int phnum)
+void wait(int s)
 {
-
-	sem_wait(&mutex);
-
-	// state that hungry
-	state[phnum] = HUNGRY;
-
-	printf("Philosopher %d is Hungry\n", phnum + 1);
-
-	// eat if neighbours are not eating
-	test(phnum);
-
-	sem_post(&mutex);
-
-	// if unable to eat wait to be signalled
-	sem_wait(&S[phnum]);
-
-	sleep(1);
-}
-
-// put down chopsticks
-void put_fork(int phnum)
-{
-
-	sem_wait(&mutex);
-
-	// state that thinking
-	state[phnum] = THINKING;
-
-	printf("Philosopher %d putting fork %d and %d down\n",
-		phnum + 1, LEFT + 1, phnum + 1);
-	printf("Philosopher %d is thinking\n", phnum + 1);
-
-	test(LEFT);
-	test(RIGHT);
-
-	sem_post(&mutex);
-}
-
-void* philosopher(void* num)
-{
-
-	while (1) {
-
-		int* i = num;
-
-		sleep(1);
-
-		take_fork(*i);
-
-		sleep(0);
-
-		put_fork(*i);
-	}
+    if ((chop[s] == 0) && (chop[(s + 1) % 5] == 0))
+    {
+        p[s] = 1;
+        chop[s] = 1;
+        chop[(s + 1) % 5] = 1;
+    }
+    else if (p[s] == 1)
+    {
+        char status;
+        printf("Do Philosopher %d wants to stop eating ? \n", s);
+        scanf("%s", &status);
+        if ((status == "Y") || (status == "y"))
+            signal(s);
+    }
+    else
+    {
+        printf("Chopstick %d and %d are busy\n", s - 1, ((s - 1) + 1) % 5);
+    }
 }
 
 int main()
 {
+    char status;
 
-	int i;
-	pthread_t thread_id[N];
-
-	// initialize the semaphores
-	sem_init(&mutex, 0, 1);
-
-	for (i = 0; i < N; i++)
-
-		sem_init(&S[i], 0, 0);
-
-	for (i = 0; i < N; i++) {
-
-		// create philosopher processes
-		pthread_create(&thread_id[i], NULL,
-					philosopher, &phil[i]);
-
-		printf("Philosopher %d is thinking\n", i + 1);
-	}
-
-	for (i = 0; i < N; i++)
-
-		pthread_join(thread_id[i], NULL);
+    for (int i = 1; i <= 5; i++)
+    {
+        p[i] = 0;
+        chop[i] = 0;
+    }
+    do
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            if (p[i] == 0)
+            {
+                printf("Philosopher %d is thinking\n", i);
+                printf("\n");
+            }
+            else
+            {
+                printf("Philosopher %d is Eating\n", i);
+                printf("\n");
+            }
+        }
+        int s;
+        printf("Which Philosopher wants to eat ? \n");
+        scanf("%d", &s);
+        wait(s);
+        printf("Do you want to continue(Y/N) ? \n");
+        scanf("%s", &status);
+    } while ((status != "N") || (status != "n"));
+    return 0;
 }
