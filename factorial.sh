47bccae99de747946e580d97dfdb0b6dccf4e514
Bhavin Patil <59190105+bhavinpatil@users.noreply.github.com>
2022-12-13 12:15:06 +0530

    Update factorial.sh

--- factorial.sh
+++ factorial.sh
@@ -29,3 +29,17 @@ else
 #calling the function
 factorial $num
 fi
+
+
+echo "Enter a number"
+read num
+
+fact=1
+
+while [ $num -gt 1 ]
+do
+  fact=$((fact * num))  #fact = fact * num
+  num=$((num - 1))      #num = num - 1
+done
+
+echo $fact
